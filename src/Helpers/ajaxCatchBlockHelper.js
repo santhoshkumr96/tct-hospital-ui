243689a682d9dece9971e683791ff51612383003
santhosh kumar <santhoshkumar@santhoshs-MacBook-Pro.local>
2022-05-24 10:29:21 +0530

    fix ajax handler function bug

--- src/Helpers/ajaxCatchBlockHelper.js
+++ src/Helpers/ajaxCatchBlockHelper.js
@@ -3,13 +3,18 @@ import { TOKEN_EXPIRED } from "../config";
 
 export function errorHelper(e){
     if(typeof(e) !== 'undefined' && typeof(e.response)  !== 'undefined'){ 
-        if ( e.response.data.error.includes('Unauthorized') ){
+        if (  e.response.data.error !== undefined && e.response.data.error.includes('Unauthorized') ){
             return TOKEN_EXPIRED;
         }
         if( e.response.data.message !== undefined && e.response.data.message.includes('JWT expired') ){
             return TOKEN_EXPIRED;
         } else {
-            return e.message;
+            console.log("cionmginidfjsahgjkdsfhk")
+            if(e.response.data.message !== undefined){
+                return e.response.data.message ;
+            }
+            return e.message
+            
         }
     }
     return e.message;
